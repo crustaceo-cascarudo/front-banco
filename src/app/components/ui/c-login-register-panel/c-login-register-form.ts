147a6973b99dec9457e7145ffd38474d7bfb0fb3
Gael Yasunari Ishimi <yasunarishimi@gmail.com>
2026-01-07 12:48:47 +0100

    modify(crustaceo-cascarudo/sprints#40):front-banco/modify-login-register

--- src/app/components/ui/c-login-register-panel/c-login-register-form.ts
+++ src/app/components/ui/c-login-register-panel/c-login-register-form.ts
@@ -1,12 +1,46 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { LoginService } from '../../../services/loginService';
+import { UserLogin } from '../../../models/user/user-login';
+import { UserRegister } from '../../../models/user/user-register';
 
 @Component({
   selector: 'c-login-register-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './c-login-register-form.html',
   styleUrl: './c-login-register-form.scss',
 })
 export class CLoginRegisterform {
   @Input() isLoginMode: boolean = true;
 
+  loginService = inject(LoginService);
+
+  loginData: UserLogin = {
+    namemail: '',
+    plainPassword: ''
+  };
+
+  registerData: UserRegister = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  onLoginSubmit(form: any) {
+    if (form.valid) {
+      console.log('Login data:', this.loginData);
+      this.loginService.logIn(this.loginData.namemail, this.loginData.plainPassword);
+    }
+  }
+
+  onRegisterSubmit(form: any) {
+    if (form.valid) {
+      console.log('Register data:', this.registerData);
+      this.loginService.register(
+        this.registerData.name,
+        this.registerData.email,
+        this.registerData.password
+      );
+    }
+  }
 }
