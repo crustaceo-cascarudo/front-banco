d86cc7852f0a877270923840daca59c2b84ebbdc
ishimiG <yasunarishimi@gmail.com>
2026-01-30 00:57:20 +0100

    no mires lorenzo

--- src/app/services/loginService.ts
+++ src/app/services/loginService.ts
@@ -13,7 +13,7 @@ export class LoginService {
   http = inject(HttpClient);
   authService = inject(AuthService);
   router = inject(Router);
-  url = "https://api-store-class.ishimi.es/api";
+  url = "https://api-store-class.ishimi.es/api/";
   loginUrl = this.url + 'login';
   registerUrl = this.url + 'register';
 
